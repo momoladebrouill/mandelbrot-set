50fd8306fbebe0bfd62b0a7e8614f30f43ce3b3d
momoladebrouill <momoladebrouill@gmail.com>
2021-12-29 21:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,28 @@ f=pg.display.set_mode((500,500),pg.RESIZABLE)
 fps=pg.time.Clock()
 B=1
 itr=50
-fac=100
+fac=10
 X,Y=250,250
 depx,depy=0,0
 def draw(size=2):
     for y in range(-int(depy)-Y,Y-int(depy),size):
             for x in range(-int(depx)-X,X-int(depx),size):
-                a=x/fac
-                b=y/fac
+                a,b=x/fac,y/fac
                 ca,cb=a,b
                 for n in range(itr):
                     aa=a**2-b**2
                     bb=2*a*b
                     a=aa+ca
                     b=bb+cb
-                    if a**2+b**2>16:break
+                    if a**2+b**2>16:
+                        break
                 
                 if n==itr-1:
                     c=(0,0,0)
                 else:
                     n=sqrt(n/itr)
-                    c=hsv_to_rgb(1-n,1,255)
+                    c=(255*(1-n),0,0)
                 pg.draw.rect(f,c,((x+X,y+Y),(size,size)))
-   
     pg.display.flip()
 while B:
     for event in pg.event.get():
@@ -57,7 +56,7 @@ while B:
                 breakpoint()
             else:
                 t=time.time()
-                draw()
+                draw(10)
                 print('done',round(time.time()-t,2),'sec')
         elif event.type==pg.VIDEORESIZE:
             X=int(event.w/2)
